fe07e4e6e24f2ce51c2b976a6e124445a9527914
antomich04 <antomich04@gmail.com>
2026-05-25 21:24:16 +0300

    Change language selection UI

--- frontend/src/app/components/sidebar/sidebar.ts
+++ frontend/src/app/components/sidebar/sidebar.ts
@@ -1,11 +1,11 @@
 import { Component, inject } from '@angular/core';
-import { RouterLink, RouterLinkActive } from "@angular/router";
-import { HlmSidebarImports } from "@spartan-ng/helm/sidebar";
-import { HlmCard, HlmCardContent } from "@spartan-ng/helm/card";
-import { HlmButton } from "@spartan-ng/helm/button";
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { HlmSidebarImports } from '@spartan-ng/helm/sidebar';
+import { HlmCard, HlmCardContent } from '@spartan-ng/helm/card';
+import { HlmButton } from '@spartan-ng/helm/button';
 import { LANGUAGE_OPTIONS, AppLanguage, LanguageService } from '../../services/language.service';
 import { TranslatePipe } from '../../shared/translate.pipe';
- 
+
 @Component({
   selector: 'ls-sidebar',
   imports: [HlmSidebarImports, HlmCard, HlmCardContent, RouterLink, HlmButton, RouterLinkActive, TranslatePipe],
@@ -15,8 +15,7 @@ export class Sidebar {
   protected readonly languageService = inject(LanguageService);
   protected readonly languageOptions = LANGUAGE_OPTIONS;
 
-  changeLanguage(event: Event): void {
-    const language = (event.target as HTMLSelectElement).value as AppLanguage;
+  setLanguage(language: AppLanguage): void {
     this.languageService.setLanguage(language);
   }
 
@@ -24,13 +23,10 @@ export class Sidebar {
     if (window.innerWidth < 768) {
       //Simulates a click to close the sidebar when in mobile screen
       const trigger = document.querySelector('[hlmSidebarTrigger]') as HTMLElement;
-      
-      if(trigger){
+
+      if (trigger) {
         trigger.click();
       }
-    
     }
   }
-
 }
-
